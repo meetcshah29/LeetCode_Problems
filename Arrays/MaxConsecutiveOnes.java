004b2a0b5525aee1e8d948059f03571f9c258cc7
meetcshah29 <meetcshah29@gmail.com>
2022-09-26 10:36:06 +0100

    Updated MaxConsecutiveOnes

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -9,23 +9,23 @@ public class MaxConsecutiveOnes {
 
     public static int maxOnes(int nums[])
     {
-        int count = 0;
+        int currentCount = 0;
         int maxCount = 0;
 
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] == 1)
             {
-                count++;
+                currentCount++;
             }
             else
             {
-                maxCount = Math.max(count, maxCount);
-                count = 0;
+                maxCount = Math.max(currentCount, maxCount);
+                currentCount = 0;
             }
         }
 
-        return Math.max(count, maxCount);
+        return Math.max(currentCount, maxCount);
     }
     
 }
